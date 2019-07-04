43e11fa3b72d23346b3ee1308d7daec65f43f4b2
inchei <inchei@protonmail.com>
2019-07-04 14:28:55 +0800

    [+] update homepage

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,27 +1,12 @@
 $(function() {
-    //An Easter egg
-      if(
-        window.outerWidth - window.innerWidth > 20 || 
-        window.outerHeight - window.innerHeight > 100){
-            setTimeout('console.log("你想幹什麽？一定是圖謀不軌！");',0);
-            setTimeout('console.warn("檢測到惡意打開控制台，啓動自爆系統……");',1000);
-            setTimeout('console.warn("倒計時開始：5");',2000);
-            setTimeout('console.warn("4");',3000);
-            setTimeout('console.log("快點！再不引爆就……");',3500);
-            setTimeout('console.warn("3");',4000);
-            setTimeout('console.log("這該死的倒計時系統……可惡！");',4700);
-            setTimeout('console.warn("2");',5000);
-            setTimeout('console.error("自爆系統啓動失敗，權限成功寫入");',5500);
-            setTimeout('console.info("歡迎使用控制臺！");',6000);
-     }
-
     //Set buttons
-    $('#more').click(function() {
+    $('#more').click(() => {
         document.querySelector('#about').scrollIntoView({ behavior: 'smooth' });
+        $('#spb').text(':)');
     });
 
     //Set the background
-    var random = Math.floor(Math.random() * 11);
+    var random = Math.floor(Math.random() * 13);
 
     function checkWebp() {
         try {
@@ -34,37 +19,43 @@ $(function() {
     function setbg(type) {
         switch (random) {
             case 0:
-                $('body').css('background-image', 'url(assets/img/bg01.' + type + ')');
+                $('.bg').attr('data-ibg-bg', 'assets/img/bg01.' + type);
                 break;
             case 1:
-                $('body').css('background-image', 'url(assets/img/bg02.' + type + ')');
+                $('.bg').attr('data-ibg-bg', 'assets/img/bg02.' + type);
                 break;
             case 2:
-                $('body').css('background-image', 'url(assets/img/bg03.' + type + ')');
+                $('.bg').attr('data-ibg-bg', 'assets/img/bg03.' + type);
                 break;
             case 3:
-                $('body').css('background-image', 'url(assets/img/bg04.' + type + ')');
+                $('.bg').attr('data-ibg-bg', 'assets/img/bg04.' + type);
                 break;
             case 4:
-                $('body').css('background-image', 'url(assets/img/bg05.' + type + ')');
+                $('.bg').attr('data-ibg-bg', 'assets/img/bg05.' + type);
                 break;
             case 5:
-                $('body').css('background-image', 'url(assets/img/bg06.' + type + ')');
+                $('.bg').attr('data-ibg-bg', 'assets/img/bg06.' + type);
                 break;
             case 6:
-                $('body').css('background-image', 'url(assets/img/bg07.' + type + ')');
+                $('.bg').attr('data-ibg-bg', 'assets/img/bg07.' + type);
                 break;
             case 7:
-                $('body').css('background-image', 'url(assets/img/bg08.' + type + ')');
+                $('.bg').attr('data-ibg-bg', 'assets/img/bg08.' + type);
                 break;
             case 8:
-                $('body').css('background-image', 'url(assets/img/bg09.' + type + ')');
+                $('.bg').attr('data-ibg-bg', 'assets/img/bg09.' + type);
                 break;
             case 9:
-                $('body').css('background-image', 'url(assets/img/bg10.' + type + ')');
+                $('.bg').attr('data-ibg-bg', 'assets/img/bg10.' + type);
                 break;
             case 10:
-                $('body').css('background-image', 'url(assets/img/bg11.' + type + ')');
+                $('.bg').attr('data-ibg-bg', 'assets/img/bg11.' + type);
+                break;
+            case 11:
+                $('.bg').attr('data-ibg-bg', 'assets/img/bg12.' + type);
+                break;
+            case 12:
+                $('.bg').attr('data-ibg-bg', 'assets/img/bg13.' + type);
                 break;
         }
     }
@@ -75,13 +66,28 @@ $(function() {
         setbg(type = 'jpg');
     }
 
+    $(".bg").interactive_bg({
+        strength: 30,
+        scale: 1.05,
+        animationSpeed: "100ms",
+        contain: true,
+        wrapContent: false
+      });
+
+    $(window).resize(function() {
+      $(".ibg-bg").css({
+        width: $(window).outerWidth(),
+        height: $(window).outerHeight()
+      })
+    })
+
     //Gradient background
     $(window).scroll(function() {
         var opacity = Math.floor(document.documentElement.scrollTop / 10) + 40;
         if (opacity > 90)
             opacity = 90;
         else
-            $('main').css('background', 'rgba(20, 20, 20, .' + String(opacity) + ')');
+            $('main').css('background', 'rgba(20, 20, 20, .' + String(opacity));
     });
 
     //Go to the top of the page when refreshing
